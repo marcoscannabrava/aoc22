a7e70dc8c8d703ae8c01fb24f9e6323e1fb2e485
Marcos Cannabrava <mpcannabrava@gmail.com>
2023-01-02 13:18:46 -0800

    day 12 part 1 wip

--- src/day12.rs
+++ src/day12.rs
@@ -1,14 +1,84 @@
+use crate::helpers::read_file;
+use std::collections::{HashMap, HashSet};
+
 /* Approach:
 1. build adjacency matrix from input
 2. DFS counting steps until "E", dead-end or exceeds steps from previous path
 */
-use crate::helpers::read_file;
 
 type Grid = Vec<Vec<char>>;
 type Pos = (usize, usize);
 
-fn dfs(grid: Grid) {}
+/*DFS Algorithm:
+1. push neighbors onto stack
+2. pop stack, add to visited, add to path_count
+3. if E, dead-end or path_count >= min, reduce path_count by 1, repeat step 2
+*/
+/// Returns min path
+fn dfs(start: Pos, end: Pos, grid: Grid) -> u32 {
+    let mut stack: Vec<Pos> = vec![start];
+    let mut path_count: u32 = 0;
+    let mut min_path_count: u32 = u32::MAX;
+    let visited: &mut HashSet<Pos> = &mut HashSet::new();
+    let neighbors = |pos: Pos, visited: &HashSet<(usize, usize)>| -> Vec<Pos> {
+        let mut result = Vec::new();
+        if pos.0 > 0 {
+            result.push((pos.0 - 1, pos.1));
+        }
+        if pos.1 > 0 {
+            result.push((pos.0, pos.1 - 1));
+        }
+        if pos.0 + 1 < grid.len() {
+            result.push((pos.0 + 1, pos.1));
+        }
+        if pos.1 + 1 < grid[0].len() {
+            result.push((pos.0, pos.1 + 1));
+        }
+        result
+            .iter()
+            .filter(|x| visited.contains(x))
+            .cloned()
+            .collect()
+    };
 
+    let alphabet: HashMap<char, u32> =
+        HashMap::from_iter((10..36).map(|n| (char::from_digit(n, 36).unwrap(), n)));
+    let height_diff = |curr: &Pos, next: &Pos| -> u32 {
+        if &grid[next.0][next.1].to_string() == "E" && &grid[curr.0][curr.1].to_string() == "z" {
+            return 1;
+        } else {
+            alphabet[&grid[next.0][next.1]] - alphabet[&grid[curr.0][curr.1]]
+        }
+    };
+
+    while !stack.is_empty() {
+        let curr = stack.pop().unwrap();
+        if visited.insert(curr) {
+            path_count += 1;
+        } else {
+            path_count -= 1;
+        };
+        let n = neighbors(curr, visited)
+            .iter()
+            .filter(|x| height_diff(&curr, x) <= 1)
+            .cloned()
+            .collect::<Vec<Pos>>();
+        let dead_end = n.is_empty();
+
+        if grid[curr.0][curr.1] == 'E' {
+            min_path_count = path_count.min(min_path_count);
+            break;
+        }
+        if dead_end || path_count >= min_path_count {
+            break;
+        }
+        stack = [stack, n].concat();
+    }
+
+    min_path_count
+}
+
+/// Returns (Start, End, Grid) tuple
 fn parser(input: &str) -> (Pos, Pos, Grid) {
     let mut start: Pos = (0, 0);
     let mut end: Pos = (0, 0);
@@ -71,5 +141,11 @@ abdefghi";
         assert_eq!(grid[2][7], 'k');
         assert_eq!(grid[3][7], 'j');
         assert_eq!(grid[4][7], 'i');
+    }
+
+    #[test]
+    fn dfs() {
+        let (start, end, grid) = day12::parser(TEST_INPUT);
+        assert_eq!(day12::dfs(start, end, grid), 31);
     }
 }
